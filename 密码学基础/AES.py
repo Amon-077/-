28936bb891aa65b80b3f57c5d85eaab24e408964
Amon-077 <78158188+Amon-077@users.noreply.github.com>
2021-02-06 20:28:01 +0800

    Add files via upload
    在ubuntu运行结果
    root@ubuntu:/mnt/hgfs/CyberSecurity/AES# python3 AES.py
    原文:  This a test message
    加密:  b'9e67baf395cadc916bb1a23bb1c38efdc0c99a9cdae14e255c885c8cbbe3a3d7'
    解密:  This a test message

---
+++ 密码学基础/AES.py
@@ -0,0 +1,35 @@
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+
+def add_to_16(text):
+    if len(text.encode('utf-8'))%16:
+        add=16-len(text.encode('utf-8'))%16
+    else:
+        add=0
+    text=text+('\0'*add)
+    return text.encode('utf-8')
+
+def encrypt(text,key):
+    key=key.encode('utf-8')
+    mode=AES.MODE_CBC
+    iv=b'qqqqqqqqqqqqqqqq'
+    text=add_to_16(text)
+    cryptos=AES.new(key,mode,iv)
+    cipher_text=cryptos.encrypt(text)
+    return b2a_hex(cipher_text)
+
+def decrypt(text,key):
+    key=key.encode('utf-8')
+    iv=b'qqqqqqqqqqqqqqqq'
+    mode=AES.MODE_CBC
+    cryptos=AES.new(key,mode,iv)
+    plain_text=cryptos.decrypt(a2b_hex(text))
+    return bytes.decode(plain_text).rstrip('\0')
+
+if __name__ == '__main__':
+    str1="This a test message"
+    e=encrypt(str1,'0123456789101112')
+    d=decrypt(e,'0123456789101112')
+    print("原文: ",str1)
+    print("加密: ",e)
+    print("解密: ",d)
